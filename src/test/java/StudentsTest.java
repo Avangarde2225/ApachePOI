d13a97db391897df39a94d48d34c0c137f6bb192
TechnoStudy <info@techno.study>
2020-03-01 15:22:32 -0500

    added comments

--- src/test/java/StudentsTest.java
+++ src/test/java/StudentsTest.java
@@ -17,30 +17,28 @@ public class StudentsTest {
         System.out.println(c1  + c2 + c3 + c4 + c5 + c6);
     }
 
-
     @DataProvider(name = "studentsData")
     public Object[][] studentsData() throws IOException {
+        // Input stream from your excel file
         FileInputStream excelFile = new FileInputStream( new File("src/test/resources/students.xlsx") );
+        // using this input stream I create a Workbook object
         Workbook wb = new XSSFWorkbook( excelFile );
-
-        Sheet sh = wb.getSheet( "data" );
-        Assert.assertNotEquals( sh, null );
-
-        int rowCount = sh.getLastRowNum() - sh.getFirstRowNum();
-        System.out.println(rowCount);
-
-        Row firstRow = sh.getRow( 0 );
-        Assert.assertNotEquals( firstRow, null );
-
-        int columnCount = firstRow.getLastCellNum() - firstRow.getFirstCellNum();
-        System.out.println(columnCount);
-
-        Object[][] resultData = new Object[rowCount][columnCount];
-
+        // get the sheet called "data"
+        Sheet sheet = wb.getSheet( "data" );
+        // check that sheet "data" exists
+        Assert.assertNotEquals( sheet, null );
+        // find out the row count
+        int rowCount = sheet.getLastRowNum() - sheet.getFirstRowNum();
+        // based on row count and six column create a two dimensional array
+        Object[][] resultData = new Object[rowCount][6];
+        // iterate of rows
         for(int i = 0; i < rowCount; i++) {
-            Row currentRow = sh.getRow( i );
-            for(int j = 0; j < columnCount; j++) {
+            // get the current row
+            Row currentRow = sheet.getRow( i );
+            for(int j = 0; j < 6; j++) {
+                // get cell value from current row
                 Cell cell = currentRow.getCell( j );
+                // assign values to your two dimensional array
                 resultData[i][j] = cell.toString();
             }
         }
